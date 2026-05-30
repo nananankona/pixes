b7f171f28c947e68cab0396a8053c93367fd2f68
nananankona <konana.net@proton.me>
2026-05-31 04:25:35 +0900

    ve

--- lib/foundation/app.dart
+++ lib/foundation/app.dart
@@ -12,7 +12,7 @@ export "state_controller.dart";
 export "navigation.dart";
 
 class _App {
-  final version = "2.0.1f";
+  final version = "2.1.0f";
 
   bool get isAndroid => Platform.isAndroid;
   bool get isIOS => Platform.isIOS;
